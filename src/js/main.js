870aa720a8338538f09fdd0dcc3a80b2f5cb008e
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-12-11 00:04:53 -0800

    fix: Made transform work!

--- src/js/main.js
+++ src/js/main.js
@@ -566,10 +566,10 @@ function updatePositions() {
      */
     //window.items[i].style.transform = 'translate3d(' + (100 * phase) + 'px, 0, 0)';
     //items[i].style.transform = 'translate3d(' + ((i % 8) * 256 + (100 * phase)) + 'px, 0, 0)';
-    window.items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    //window.items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     //window.items[i].style.transform = 'translateX(' + ((i % 8) * 256 + (100 * phase)) + 'px)';
     //window.items[i].style.transform = 'translateX(' + ((i % 8) * 256 + (phase)) + 'px)';
-    //window.items[i].style.transform = 'translateX(' +  (100 * phase) + 'px)';
+    window.items[i].style.transform = 'translateX(' +  (100 * phase) + 'px)';
   }
 
 window.animating = false;
@@ -603,7 +603,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Move to css
     // elem.style.height = "100px";
     // elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
+    //elem.basicLeft = (i % cols) * s;
+    elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     movingPizzas.appendChild(elem);
     // Move this here to stop updatePositions from re-defining items on every scroll event
